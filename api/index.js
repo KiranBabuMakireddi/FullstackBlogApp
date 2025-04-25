c063ab84cf71c2a1697dd28b07803acfe8fa776d
KiranBabuMakireddi <kiranbabumakireddi@gmail.com>
2025-04-25 17:55:56 +0530

    DB connection

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import { log } from 'console';
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config(); // <-- IMPORTANT: must come before using process.env
+
 const app = express();
 
-app.listen(3000,()=>(
-   console.log('Server Runnning')
-   
-))
+mongoose.connect(process.env.MONGO)
+  .then(() => {
+    console.log("MongoDB is connected");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
+app.listen(3000, () => {
+  console.log('Server Running on port 3000');
+});
